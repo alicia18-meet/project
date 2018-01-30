af8908a8c4975e6581a53c90b2c62df20e9054b6
neta16-meet <neta16@meet.mit.edu>
2018-01-30 18:55:11 +0200

    added database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask 	
-from flask import render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "user"
+    id= db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    def __init__(self,email,psw):
+    	self.email=email
+    	self.psw=psw
+
+db.create_all()
+
 
 @app.route("/")
 @app.route("/home")
@@ -14,6 +30,25 @@ def projects():
 @app.route("/galleries")
 def galleries():
 	return render_template('galleries.html')
+
+@app.route("/staff")
+def staff():
+	return render_template('staff.html')
+
+@app.route("/signup", methods= ["GET","POST"])
+def signup():
+	if request.method=="GET":
+		return render_template('signup.html')
+	else:
+		email = request.form['email']
+		psw = request.form['psw']
+		x=User(email,psw)
+		db.session.add(x)
+		db.session.commit()
+		return render_template('aftersignup.html')
+
+
+
 
 if __name__ == '__main__':
 	app.run()
